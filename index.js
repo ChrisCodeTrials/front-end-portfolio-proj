8a732ad66f1fc71539a515ff759cb38122d3a18e
chris.miranda <christophermiranda@pursuit.org>
2023-11-18 23:25:35 -0500

    added random movies on-page-load

--- index.js
+++ index.js
@@ -30,20 +30,54 @@ function getGenreByValue(value) {
     }
 }
 
-let pageNumber = 2
+let pageNumber = 1
 
+function getRandomGenreAndRating() {
+    var genreOptions = [
+        { value: "", text: "--Genres--" },
+        { value: 28, text: "Action" },
+        { value: 12, text: "Adventure" },
+        { value: 16, text: "Animation" },
+        { value: 35, text: "Comedy" },
+        { value: 80, text: "Crime" },
+        { value: 99, text: "Documentary" },
+        { value: 18, text: "Drama" },
+        { value:10751, text: "Family" },
+        { value: 14, text: "Fantasy" },
+        { value: 36, text: "History" },
+        { value: 27, text: "Horror" },
+        { value: 10402, text: "Music" },
+        { value: 9648, text: "Mystery" },
+        { value: 10749, text: "Romance" },
+        { value: 878, text: "Science Fiction" },
+        { value: 10770, text: "TV Movie" },
+        { value: 53, text: "Thriller" },
+        { value: 10752, text: "War" },
+        { value: 37, text: "Western" }
+    ];
+
+    var ratingOptions = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+    const randomGenreIndex = Math.floor(Math.random() * genreOptions.length)
+    const randomRatingIndex = Math.floor(Math.random() * ratingOptions.length)
+
+    return {
+        genre: genreOptions[randomGenreIndex].value,
+        rating: ratingOptions[randomRatingIndex]
+    }
+}
 
 const form = document.querySelector("form")
 const main = document.querySelector("main")
 const showMoreButton = document.querySelector('.show-more-button');
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+    event.preventDefault() 
+    main.innerHTML = ""
 
-    const genreSelect = form.elements["genre"];
-    const languageSelect = form.elements["language-sel"]
-    const ratingInput = form.elements["rating-input"]
-    
+    const genreSelect = event.target.elements["genre"]
+    const languageSelect = event.target.elements["language-sel"]
+    const ratingInput = event.target.elements["rating-input"]
 
     const genreId = genreSelect.value
     const originalLanguage = languageSelect.value
@@ -53,21 +87,18 @@ form.addEventListener("submit", (event) => {
 })
 
 showMoreButton.addEventListener("click", (event) => {
-    
-    const genreSelect = form.elements["genre"];
-    const languageSelect = form.elements["language-sel"]
-    const ratingInput = form.elements["rating-input"]
-    
+    const genreSelect = event.target.form.elements["genre"]
+    const languageSelect = event.target.form.elements["language-sel"]
+    const ratingInput = event.target.form.elements["rating-input"]
 
     const genreId = genreSelect.value
     const originalLanguage = languageSelect.value
-    const userRating = ratingInput.value 
+    const userRating = ratingInput.value
 
     fetchMovies(genreId, originalLanguage, userRating, pageNumber)
     pageNumber++
-
-
 })
+
 
 function displayMovies(movieList) {
 
@@ -96,6 +127,19 @@ function displayMovies(movieList) {
 
 }
 
+function errorMessage(error){
+    const divError = document.querySelector(".error-message")
+    divError.style.display= "block"
+
+    divError.innerHTML= `<h1>There was an issue accessing the API! 🙀<h1>
+    <p id="error-message">${error}</p>`
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const { genre: randomGenreId, rating: randomRating } = getRandomGenreAndRating()
+
+    fetchMovies(randomGenreId, 'en', randomRating)
+})
 
 function fetchMovies(genreId, originalLanguage, userRating) {
     const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=${originalLanguage}&page=${pageNumber}&sort_by=popularity.desc&vote_average.gte=${userRating}&with_genres=${genreId}`
